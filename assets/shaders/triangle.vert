12429fab6eede9629797c20f8b2f6868b150a5a3
ZiadSheriif <zsherif308@gmail.com>
2023-03-17 03:12:00 +0200

    feat: add vertex shader of triangle

--- assets/shaders/triangle.vert
+++ assets/shaders/triangle.vert
@@ -5,6 +5,9 @@
 // And it also should send the vertex color as a varying to the fragment shader where the colors are (in order):
 // (1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)
 
+#define FIRST_VERTEX_COLOR 0
+#define SECOND_VERTEX_COLOR 1
+
 out Varyings {
     vec3 color;
 } vs_out;
@@ -14,7 +17,30 @@ out Varyings {
 // Each vertex "v" should be transformed to be "scale * v + translation".
 // The default value for "translation" is (0.0, 0.0) and for "scale" is (1.0, 1.0).
 
+// define the uniforms for translation and scale
+uniform vec2 translation = vec2(0.0, 0.0);
+uniform vec2 scale = vec2(1.0, 1.0);
+
 //TODO: (Req 1) Finish this shader
 
-void main(){
+void main() {
+    // Define the vertices of the triangle in normalized device coordinates
+    vec3 vertices[3] = vec3[3](vec3(-0.5, -0.5, 0.0), vec3(0.5, -0.5, 0.0), vec3(0.0, 0.5, 0.0));
+
+    // define the positions of the triangle in normalized device coordinates
+    vec3 position = vec3(scale * vertices[gl_VertexID].xy + translation, 1.0);
+
+    // assign color to each vertex in the triangle
+    if(gl_VertexID == FIRST_VERTEX_COLOR) {
+        vs_out.color = vec3(1.0, 0.0, 0.0); // set to red
+
+    } else if(gl_VertexID == SECOND_VERTEX_COLOR) {
+        vs_out.color = vec3(0.0, 1.0, 0.0); // set to green
+
+    } else {
+        vs_out.color = vec3(0.0, 0.0, 1.0); // set to blue
+    }
+
+    // Set the position of the current vertex
+    gl_Position = vec4(position, 1.0);
 }
--- source/common/shader/shader.hpp
+++ source/common/shader/shader.hpp
@@ -65,25 +65,36 @@ namespace our
         void set(const std::string &uniform, glm::vec2 value)
         {
             // TODO: (Req 1) Send the given 2D vector value to the given uniform
+            glUniform2fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
 
         void set(const std::string &uniform, glm::vec3 value)
         {
             // TODO: (Req 1) Send the given 3D vector value to the given uniform
+            glUniform3fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
 
         void set(const std::string &uniform, glm::vec4 value)
         {
             // TODO: (Req 1) Send the given 4D vector value to the given uniform
+            glUniform4fv(getUniformLocation(uniform), 1, glm::value_ptr(value));
         }
 
         void set(const std::string &uniform, glm::mat4 matrix)
         {
             // TODO: (Req 1) Send the given matrix 4x4 value to the given uniform
+            glUniformMatrix4fv(getUniformLocation(uniform), 1, false, &matrix[0][0]);
         }
 
         // TODO: (Req 1) Delete the copy constructor and assignment operator.
+        ShaderProgram(const ShaderProgram &) = delete;
+        ShaderProgram &operator=(const ShaderProgram &) = delete;
         // Question: Why do we delete the copy constructor and assignment operator?
+        // ? to prevent accidental copying of the program handle.
+        // ? The program handle is an OpenGLobject name, which is not copyable.
+        // ? If the copy constructor or assignment operator were allowed,the program handle would be copied,
+        // ? which could cause problems such as two objects pointing to the same OpenGL program,
+        // ? causing unexpected behavior or even crashes.
     };
 
 }
