aa09ceaf9e2dd02484bca6c5bc75e4caad071d89
medo emad <medo.emadd23@gmail.com>
2023-05-09 19:09:32 +0200

    comment the printing lines

--- source/common/systems/car-generator.hpp
+++ source/common/systems/car-generator.hpp
@@ -34,10 +34,10 @@ namespace our
                     // Change the position and rotation based on the linear & angular velocity and delta time.
                     if (movement->name == "car" ) {
 
-                        std::cout << entity->localTransform.position[1] << std::endl;
+                        // std::cout << entity->localTransform.position[1] << std::endl;
                         if (-1.2f <= entity->localTransform.position[1] && entity->localTransform.position[1] <= 1.6f)
                         {
-                            std::cout << "nside" << std::endl;
+                            // std::cout << "nside" << std::endl;
                             entity->localTransform.position += deltaTime * movement->linearVelocity;
                             if (entity->localTransform.position[1] <= 0.0f)
                             {
@@ -48,19 +48,19 @@ namespace our
                                 // movement->linearVelocity[1] = -0.5f;
                                 int  sID =  std::stoi(movement->id);
                                 int searchId = sID % 2 == 0 ? sID - 1 : sID + 1;
-                                std::cout << "searchId" << searchId << std::endl;
+                                // std::cout << "searchId" << searchId << std::endl;
                                 for (auto entity2 : world->getEntities()){
                                     MovementComponent *movement2 = entity2->getComponent<MovementComponent>();
                                     if(movement2 && movement2->name == "car") {
                                     int  sID =  std::stoi( movement2->id);
                                     if (sID == searchId && entity2->localTransform.position[1] == 1.2f) {
-                                        std::cout << "found" << searchId << std::endl;
+                                        // std::cout << "found" << searchId << std::endl;
                                         srand(time(nullptr));  // seed the random number generator with the current time
                                         // generate a random number between 1 and 3
                                         int random_num = rand() % 8 + 1;
                                         movement->linearVelocity[1] = -0.1f * random_num;
                                         movement2->linearVelocity[1] = -0.1f * random_num;
-                                        std::cout << "random " << random_num << std::endl;
+                                        // std::cout << "random " << random_num << std::endl;
                                         break;
                                     }
                                     }
@@ -70,7 +70,7 @@ namespace our
                         else
                         {
                             // inside the width
-                            std::cout << "start" << std::endl;
+                            // std::cout << "start" << std::endl;
                             entity->localTransform.position[1] = 1.2f;
                             // movement->linearVelocity[1] = 0.0f;
                             movement->linearVelocity[1] = 0.0;
